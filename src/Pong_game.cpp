dae7148845acaa0acf9c281a9771210035eab744
Siri Persson <siri.persson@sylog.se>
2018-06-15 12:33:50 +0100

    Project setup

---
+++ src/Pong_game.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Pong game
+// Author      : Siri and Rasmus
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Pong game" << endl;
+	return 0;
+}
