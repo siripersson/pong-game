c95afc0dafa52add40444a338dcd86cd86dbe255
Siri Persson <siri.persson@sylog.se>
2018-06-18 08:52:29 +0100

    Add SDL2 libraries

--- src/Pong_game.cpp
+++ src/Pong_game.cpp
@@ -6,10 +6,27 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
-using namespace std;
+#include <SDL.h>
 
-int main() {
-	cout << "Pong game" << endl;
-	return 0;
+int main(int argc, char* argv[]) {
+    // Start SDL2
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    // Create a Window in the middle of the screen
+    SDL_Window *window = 0;
+
+    window = SDL_CreateWindow("Hello World!",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              640, 480,
+                              SDL_WINDOW_SHOWN);
+
+    // Delay so that we can see the window appear
+    SDL_Delay(2000);
+
+    // Cleanup and Quit
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
